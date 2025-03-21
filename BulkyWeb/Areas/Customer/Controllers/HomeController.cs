c8f6ee8384d7eedc6ca35a6b5d66be90726c5eb0
kevinpham12188 <kevinpham7788@gmail.com>
2025-03-21 14:26:46 -0700

    Implement Home Page UI

--- BulkyWeb/Areas/Customer/Controllers/HomeController.cs
+++ BulkyWeb/Areas/Customer/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Bulky.Models;
+using Bulky.DataAccess.Repository;
+using Bulky.DataAccess.Repository.IRepository;
 
 namespace BulkyWeb.Areas.Customer.Controllers;
 [Area("Customer")]
@@ -8,15 +10,18 @@ namespace BulkyWeb.Areas.Customer.Controllers;
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly IUnitOfWork _unitOfWork;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(IUnitOfWork unitOfWork, ILogger<HomeController> logger)
     {
         _logger = logger;
+        _unitOfWork = unitOfWork;
     }
 
     public IActionResult Index()
     {
-        return View();
+        IEnumerable<Product> productList = _unitOfWork.Product.GetAll(includeProperties: "Category");
+        return View(productList);
     }
 
     public IActionResult Privacy()
