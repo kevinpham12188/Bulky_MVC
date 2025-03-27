d2925df78d0f46fe733b033c7616177c8ed8ede2
kevinpham12188 <kevinpham7788@gmail.com>
2025-03-27 10:25:20 -0700

    Configure Order Header and Order Detail Repos

--- Bulky.DataAccess/Repository/UnitOfWork.cs
+++ Bulky.DataAccess/Repository/UnitOfWork.cs
@@ -17,6 +17,8 @@ namespace Bulky.DataAccess.Repository
         public ICompanyRepository Company { get; private set; }
         public IShoppingCartRepository ShoppingCart { get; private set; }
         public IApplicationUserRepository ApplicationUser { get; private set; }
+        public IOrderHeaderRepository OrderHeader { get; private set; }
+        public IOrderDetailRepository OrderDetail { get; private set; }
 
 
         public UnitOfWork(ApplicationDbContext db)
@@ -27,6 +29,8 @@ namespace Bulky.DataAccess.Repository
             Product = new ProductRepository(_db);
             Company = new CompanyRepository(_db);
             ShoppingCart = new ShoppingCartRepository(_db);
+            OrderHeader = new OrderHeaderRepository(_db);
+            OrderDetail = new OrderDetailRepository(_db);
         }
 
         public void Save()
