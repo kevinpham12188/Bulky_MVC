90139576d675e77acfff5148afdfc9d95ee46b84
kevinpham12188 <kevinpham7788@gmail.com>
2025-04-03 23:39:44 -0700

    Display Image in Shopping Cart

--- BulkyWeb/Areas/Customer/Controllers/HomeController.cs
+++ BulkyWeb/Areas/Customer/Controllers/HomeController.cs
@@ -24,7 +24,7 @@ public class HomeController : Controller
 
     public IActionResult Index()
     {
-        IEnumerable<Product> productList = _unitOfWork.Product.GetAll(includeProperties: "Category");
+        IEnumerable<Product> productList = _unitOfWork.Product.GetAll(includeProperties: "Category,ProductImages");
         return View(productList);
     }
 
@@ -32,7 +32,7 @@ public class HomeController : Controller
     {
         ShoppingCart cart = new()
         {
-            Product = _unitOfWork.Product.Get(u => u.Id == productId, includeProperties: "Category"),
+            Product = _unitOfWork.Product.Get(u => u.Id == productId, includeProperties: "Category,ProductImages"),
             Count = 1,
             ProductId = productId
 
